16f9d729b5eb759796c9095baf66ef536320b2f9
Andrey Nikonov <a.einomi@gmail.com>
2021-03-01 15:27:25 +0300

    Change image settings

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,7 @@ const Image = require('@11ty/eleventy-img');
 const { RemoteAssetCache } = require('@11ty/eleventy-cache-assets');
 const sizeOf = require('image-size');
 
-async function imageShortcode(src, attributes = {}, maxWidth = 1440) {
-  console.log('maxWidth', maxWidth);
+async function imageShortcode(src, attributes = {}, maxWidth = 2636) {
   if (typeof src != 'string') {
     throw new Error(`The path for the image is incorrect: ${src}`);
   }
@@ -16,17 +15,28 @@ async function imageShortcode(src, attributes = {}, maxWidth = 1440) {
     throw new Error('Image attributes should be of type `object`');
   }
 
+  const widths = [295, 590, 1180, 1770, 2360];
+
   const options = {
-    widths: [...[300, 600, 1000].filter((v) => maxWidth - v >= 200), maxWidth],
+    widths: [...widths.filter((v) => maxWidth - v >= 200), maxWidth],
     formats: ['avif', 'webp', 'jpeg'],
     urlPath: '/assets/images/',
     outputDir: './dist/assets/images/',
   };
 
   const metadata = await Image(src, options);
+  const firstMetadataObj = metadata[Object.keys(metadata)[0]];
+  const maxWidthReal = firstMetadataObj.reduce((acc, curr) => {
+    if (curr.width > acc) {
+      return curr.width;
+    }
+    return acc;
+  }, 0);
+
+  console.log('metadata', metadata);
 
   let imageAttributes = {
-    sizes: `(max-width: ${maxWidth}px) 100vw, ${maxWidth}px`,
+    sizes: `(max-width: ${maxWidthReal}px) 100vw, ${maxWidthReal}px`,
     alt: '',
     loading: 'lazy',
     decoding: 'async',
